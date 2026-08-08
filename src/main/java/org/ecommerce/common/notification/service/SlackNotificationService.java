b80465ba05da261e132f2e8b30c1e00e8449fdc8
ma3llim <mohdsameer68257@gmail.com>
2026-08-08 21:03:55 +0530

    feat: created two templeted for emails

--- src/main/java/org/ecommerce/common/notification/service/SlackNotificationService.java
+++ src/main/java/org/ecommerce/common/notification/service/SlackNotificationService.java
@@ -1,4 +1,12 @@
 package org.ecommerce.common.notification.service;
 
-public class SlackNotificationService {
+import org.ecommerce.common.notification.dtos.request.NotificationRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SlackNotificationService implements NotificationService {
+    @Override
+    public void send(NotificationRequest request) {
+        
+    }
 }
