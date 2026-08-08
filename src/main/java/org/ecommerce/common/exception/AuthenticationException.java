a15711ee8d10306971dfe4c72811ba60cb958d9f
ma3llim <mohdsameer68257@gmail.com>
2026-08-08 18:41:43 +0530

    feat: remove extra exception

--- src/main/java/org/ecommerce/common/exception/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package org.ecommerce.common.exception;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
