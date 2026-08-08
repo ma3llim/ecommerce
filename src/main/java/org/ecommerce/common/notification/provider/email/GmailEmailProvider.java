da157312220378625769d30a535d201933fab287
ma3llim <mohdsameer68257@gmail.com>
2026-08-08 23:43:13 +0530

    feat: added the logs in gmail provider

--- src/main/java/org/ecommerce/common/notification/provider/email/GmailEmailProvider.java
+++ src/main/java/org/ecommerce/common/notification/provider/email/GmailEmailProvider.java
@@ -3,12 +3,14 @@ package org.ecommerce.common.notification.provider.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class GmailEmailProvider implements EmailProvider {
     private final JavaMailSender mailSender;
 
@@ -22,11 +24,13 @@ public class GmailEmailProvider implements EmailProvider {
             helper.setText(htmlContent, true);
 
             mailSender.send(message);
+            log.info("Email provider send operation completed successfully");
         } catch (MessagingException exception) {
-            throw new IllegalStateException(
-                    "Failed to send email to recipient",
-                    exception
-            );
+            log.error("Email provider failed while preparing email message", exception);
+            throw new IllegalStateException("Failed to send email notification", exception);
+        } catch (Exception exception) {
+            log.error("Email provider failed while sending email", exception);
+            throw new IllegalStateException("Failed to send email notification", exception);
         }
     }
 }
