fa8ef9e06fa48aa4c79bd0cbb53f8ab45c02b0ab
ma3llim <mohdsameer68257@gmail.com>
2026-08-10 10:44:46 +0530

    refactor: remove extra code

--- src/main/java/org/ecommerce/common/config/security/SecurityConfig.java
+++ src/main/java/org/ecommerce/common/config/security/SecurityConfig.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.ecommerce.auth.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -49,11 +47,5 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 }
