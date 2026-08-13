8d4b3b1222bfaa6bb9b10f5caf27f997a21c508d
ma3llim <mohdsameer68257@gmail.com>
2026-08-13 22:34:14 +0530

    feat: added get all caetgories api

--- src/main/java/org/ecommerce/catelog/controller/admin/AdminCategoryController.java
+++ src/main/java/org/ecommerce/catelog/controller/admin/AdminCategoryController.java
@@ -6,14 +6,15 @@ import lombok.RequiredArgsConstructor;
 import org.ecommerce.catelog.dtos.admin.request.AddCategoryRequestDto;
 import org.ecommerce.catelog.dtos.admin.response.CategoryResponse;
 import org.ecommerce.catelog.service.admin.AdminCategoryService;
+import org.ecommerce.common.dtos.PageResponse;
 import org.ecommerce.common.response.ApiSuccessResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/admin/categories")
 public class AdminCategoryController {
     private final AdminCategoryService adminCategoryService;
-
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiSuccessResponse<CategoryResponse>> createCategory(
@@ -38,9 +38,19 @@ public class AdminCategoryController {
     }
 
 
-//    @GetMapping
-//    public ResponseEntity<?> getAllCategories() {
-//    }
+    @GetMapping
+    public ResponseEntity<ApiSuccessResponse<PageResponse<CategoryResponse>>> getAllCategories(
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            HttpServletRequest request) {
+        PageResponse<CategoryResponse> allCategories = adminCategoryService.getAllCategories(pageable);
+
+        return ResponseEntity.ok(
+                ApiSuccessResponse.<PageResponse<CategoryResponse>>builder()
+                        .success(true).message("Get all category")
+                        .data(allCategories)
+                        .path(request.getRequestURI()).build()
+        );
+    }
 //    @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 //    public ResponseEntity<?> updateCategory(@PathVariable UUID id) {
 //    }
--- src/main/java/org/ecommerce/catelog/service/admin/AdminCategoryService.java
+++ src/main/java/org/ecommerce/catelog/service/admin/AdminCategoryService.java
@@ -7,10 +7,13 @@ import org.ecommerce.catelog.dtos.admin.response.CategoryResponse;
 import org.ecommerce.catelog.entities.Category;
 import org.ecommerce.catelog.repository.CategoryRepository;
 import org.ecommerce.common.dtos.CloudinaryUploadResult;
+import org.ecommerce.common.dtos.PageResponse;
 import org.ecommerce.common.enums.CloudinaryFolder;
 import org.ecommerce.common.exception.ResourceAlreadyExistsException;
 import org.ecommerce.common.service.CloudinaryService;
 import org.ecommerce.common.utils.SlugUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -39,5 +42,22 @@ public class AdminCategoryService {
 
         Category savedCategory = categoryRepository.save(newCategoryEntity);
         return objectMapper.convertValue(savedCategory, CategoryResponse.class);
+    }
+
+    public PageResponse<CategoryResponse> getAllCategories(Pageable pageable) {
+        Page<Category> categories = categoryRepository.findAll(pageable);
+
+        Page<CategoryResponse> categoriesInfoResponse = categories.map(
+                category -> objectMapper.convertValue(category, CategoryResponse.class));
+
+        return new PageResponse<>(
+                categoriesInfoResponse.getContent(),
+                categoriesInfoResponse.getNumber(),
+                categoriesInfoResponse.getSize(),
+                categoriesInfoResponse.getTotalElements(),
+                categoriesInfoResponse.getTotalPages(),
+                categoriesInfoResponse.isFirst(),
+                categoriesInfoResponse.isLast()
+        );
     }
 }
