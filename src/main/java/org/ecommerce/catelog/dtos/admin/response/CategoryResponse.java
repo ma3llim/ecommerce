b5b22fda343dc040d96bbe9e3fff33f7909f2adc
ma3llim <mohdsameer68257@gmail.com>
2026-08-17 18:21:33 +0530

    refactor: record field name

--- src/main/java/org/ecommerce/catelog/dtos/admin/response/CategoryResponse.java
+++ src/main/java/org/ecommerce/catelog/dtos/admin/response/CategoryResponse.java
@@ -1,10 +1,13 @@
 package org.ecommerce.catelog.dtos.admin.response;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import java.time.Instant;
 import java.util.UUID;
 
 public record CategoryResponse(
-        UUID id,
+        @JsonAlias("id")
+        UUID categoryId,
         String name,
         String slug,
         String imageUrl,
